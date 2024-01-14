2842cb635ad48fed901c8e19049eac51a4ec219b
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2024-01-14 11:34:28 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"hello": "world",
+		})
+	})
+
+	app.Listen(":8000")
+}
